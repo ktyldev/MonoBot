f92024984af73a0748e0afb2929d7ce7422168aa
Cat Flynn <mewtwo97@hotmail.com>
2017-10-04 01:42:08 +0100

    Add git gud command

---
+++ MonoBot/Commands.cs
@@ -0,0 +1,31 @@
+﻿using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Links : ModuleBase {
+
+    private Link[] _links = new Link[] {
+            new Link {
+                Name = "git gud",
+                Url = "https://docs.google.com/document/d/14c_HM8c2znmiFB00whNEYekx6LKyvF99XnE6BuTzwXI/edit?usp=sharing"
+            }
+        };
+
+    [Command("link")]
+    [Summary("A quick and easy way to share commonly used links :)")]
+    public async Task Link([Remainder()] string linkName) {
+        var link = _links.SingleOrDefault(l => l.Name == linkName);
+        if (link != null)
+            await ReplyAsync(link.Url);
+
+        await Context.Message.DeleteAsync();
+    }
+}
+
+public class Link {
+    public string Name { get; set; }
+    public string Url { get; set; }
+}
