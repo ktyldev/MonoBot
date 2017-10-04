ed00259e1f6999d8c857b4a3dabd281e8a312b4d
Cat Flynn <mewtwo97@hotmail.com>
2017-10-04 03:01:19 +0100

    Move links to config file

--- MonoBot/Commands.cs
+++ MonoBot/Commands.cs
@@ -1,28 +1,34 @@
 ﻿using Discord.Commands;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 public class Links : ModuleBase {
 
-    private Link[] _links = new Link[] {
-            new Link {
-                Name = "git gud",
-                Url = "https://docs.google.com/document/d/14c_HM8c2znmiFB00whNEYekx6LKyvF99XnE6BuTzwXI/edit?usp=sharing"
-            }
-        };
-
+    private string _configPath = "..\\..\\config.json";
+    
     [Command("link")]
     [Summary("A quick and easy way to share commonly used links :)")]
-    public async Task Link(string linkName) {
-        var link = _links.SingleOrDefault(l => l.Name == linkName);
+    public async Task Link([Remainder] string linkName) {
+        var link = GetLinks().SingleOrDefault(l => l.Name == linkName);
         if (link != null)
             await ReplyAsync(link.Url);
 
         await Context.Message.DeleteAsync();
     }
+
+    private Link[] GetLinks() {
+        var config = JsonConvert.DeserializeObject<Config>(File.ReadAllText(_configPath));
+        return config.Links.ToArray();
+    }
+}
+
+public class Config {
+    public List<Link> Links { get; set; }
 }
 
 public class Link {
